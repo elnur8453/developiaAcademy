dd9bb9128da29584ceaec2c2334cd8d5dde0fafc
elnurjalilov <92107436+elnur8453@users.noreply.github.com>
2024-04-25 19:16:53 +0400

    init

---
+++ src/VariablesPractice.java
@@ -0,0 +1,18 @@
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("age: " + age);
+
+        boolean hasHome = true;
+        System.out.println("hasHome: " + hasHome);
+
+        char studentGrade = 'A';
+        System.out.println("studentGrade: " + studentGrade);
+
+        double productPrice = 3.8;
+        System.out.println("productPrice: " + productPrice);
+
+        String studentName = "Yaqub";
+        System.out.println("studentName: " + studentName);
+    }
+}
