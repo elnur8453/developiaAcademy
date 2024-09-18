cd724ef98a2c2e41eaf39a5f6d9fc4464d570e96
elnurjalilov <92107436+elnur8453@users.noreply.github.com>
2024-09-19 02:12:11 +0400

    library project created

---
+++ myprojects/library-elnur/src/main/java/az/developia/library_elnur/LibraryElnurApplication.java
@@ -0,0 +1,13 @@
+package az.developia.library_elnur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryElnurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryElnurApplication.class, args);
+	}
+
+}
---
+++ myprojects/library-elnur/src/test/java/az/developia/library_elnur/LibraryElnurApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.library_elnur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryElnurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
