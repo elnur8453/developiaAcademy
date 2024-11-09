054df08593a20a04260377473f9522ab3c6f1aa0
Elnur Celilli <92107436+elnur8453@users.noreply.github.com>
2024-11-09 20:43:28 +0400

    balance management project created

---
+++ end-project/balance-management/src/main/java/az/developia/balance_management/BalanceManagementApplication.java
@@ -0,0 +1,13 @@
+package az.developia.balance_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceManagementApplication.class, args);
+	}
+
+}
---
+++ end-project/balance-management/src/test/java/az/developia/balance_management/BalanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.balance_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
